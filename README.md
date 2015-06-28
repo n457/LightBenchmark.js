7e3c1d025e17e238ed79a44e2aff872c7a37604c
n457 <n457.contact@gmail.com>
2015-06-28 20:13:12 +0200

    v.1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # LightTimer.js
-A simple and lightweight timer in native JavaScript. Useful for benchmarks.
+
+A **simple** and lightweight **timer in native JavaScript**. Useful for **benchmarks**.
+
+![](doc/assets/screenshot.png)
+
+
+
+### Usage
+
+LightTimer.js is written in native JavaScript, so it **doesn't need any dependency**.
+Just include [lighttimer.js](lighttimer.js) or [lighttimer.min.js](lighttimer.min.js) in `<header></header>` **or just before** `</body>` (**recommended**).
+
+```html
+  <script src="/your-custom-path/lighttimer.min.js"></script>
+  <script src="/another-path/your-script.js"></script>
+</body>
+```
+
+
+
+Then, you can call the **lighttimer function** in different ways.
+
+The **most complete and useful way** is :
+
+```javascript
+var options = {name: 'name of the timer', loops: 'default'};
+lighttimer(options, function (ltIncrement, ltOptions) {
+  // Your instructions.
+});
+```
+
+`name` must be a `string`. Default value : `'Default'`.
+
+`loops` must be a `integer` or a **predefined value** :
+
+* `'low'` : `1000` loops.
+* `'default'` : `10000` loops. **Default value**.
+* `'high'` : `100000` loops.
+
+You don't necessarily have to specify `name` or `loops`, but they will be **replaced by default values**.
+
+
+
+You can also access some **additional datas** by specify them as **arguments** of the function you give to lighttimer(). **You can give them the names you want** :
+
+```javascript
+lighttimer({}, function (ltIncrement, ltOptions) {});
+```
+
+* `ltIncrement` : increment variable.
+* `ltOptions` : options you gave, composed of `ltOptions.name` and `ltOptions.loops`.
+
+
+
+So, the **minimal way** to call lighttimer() is :
+
+```javascript
+lighttimer({}, function () {
+  // Your instructions.
+});
+```
+
+
+
+### Notes
+
+* More the `loops` value is **high**, more the results will be **precise and accurate**.
+
+* **The execution time may vary** depending on the **computer** and the **browser** used.
+
+* If you like LightTimer.js, please ![](doc/assets/star-repo.png) this repository so it can **easily be found** and **help other developers**.
+
+**Thank you !** :wink:
+
+
+
+### License
+LightTimer.js is released under the [MIT License](LICENSE).
---
+++ lighttimer.js
@@ -0,0 +1,74 @@
+// LightTimer.js - A simple and lightweight timer in native JavaScript. Useful for benchmarks.
+// Version 1.0
+// MIT License
+// Copyright (c) 2015 Bertrand Vignaud-Lerouge / n457 - https://github.com/n457
+
+
+// options : object
+// callback : function
+function lighttimer(options, callback, scope) {
+
+  'use strict';
+
+  var lowLoops = 1000,
+      defaultLoops = 10000,
+      highLoops = 100000;
+
+  if (! options) {
+    console.error('Fatal error : missing options.');
+    return;
+  } else if (typeof options !== 'object') {
+    options = {};
+    console.warn('Warning : incorrect options. Redefined as an object. May cause errors.');
+  }
+
+  if ( ! options.name || typeof options.name !== 'string') {
+    options.name = 'Default';
+    console.info('Info : missing or incorrect name. "' + options.name + '" name given instead.');
+  }
+
+  if ( ! options.loops) {
+    options.loops = defaultLoops;
+    console.info('Info : missing "loops" value. Default value given : ' + defaultLoops + '.');
+  } else if (typeof options.loops === 'number' || typeof options.loops === 'string') {
+    var checkInt = parseInt(options.loops);
+    if (checkInt) {
+      options.loops = checkInt;
+    } else {
+      switch (options.loops) {
+        case 'low':
+          options.loops = lowLoops;
+          break;
+        case 'default':
+          options.loops = defaultLoops;
+          break;
+        case 'high':
+          options.loops = highLoops;
+          break;
+        default:
+          options.loops = defaultLoops;
+          console.info('Info : incorrect "loops" value. Default value given instead : ' + defaultLoops + '.');
+          break;
+      }
+    }
+  } else {
+    options.loops = defaultLoops;
+    console.info('Info : incorrect "loops" value. Default value given instead : ' + defaultLoops + '.');
+  }
+
+  if ( ! callback || typeof callback !== 'function') {
+    console.error('Fatal error : missing or incorrect callback function.');
+    return;
+  }
+
+
+  console.info('"' + options.name + '" code is tested ' + options.loops + ' times :');
+  console.time(options.name);
+
+  for (var i = 0; i < options.loops; i++) {
+    callback.call(scope, i, options);
+  }
+
+  console.timeEnd(options.name);
+  console.log('\n');
+}
